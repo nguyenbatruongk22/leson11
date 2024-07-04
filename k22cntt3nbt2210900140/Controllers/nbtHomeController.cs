3f5e55d15d405a0e7c3cadf0b97ebad2f2e90dc1
nguyenbatruongk22 <chanhchua1301@gmail.com>
2024-07-04 11:32:05 +0700

    ls11

---
+++ k22cntt3nbt2210900140/Controllers/nbtHomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace k22cntt3nbt2210900140.Controllers
+{
+    public class nbtHomeController : Controller
+    {
+        public ActionResult nbtIndex()
+        {
+            return View();
+        }
+
+        public ActionResult nbtAbout()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult nbtContact()
+        {
+            ViewBag.msv = "2210900140.";
+            ViewBag.fullname = "nguyen ba truong";
+
+            return View();
+        }
+    }
+}
